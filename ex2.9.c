63adc8509f641e7f7acd819dcb85b2cab1284e66
Atul Kumar <atulkumartata@gmail.com>
2018-01-02 01:11:54 +0530

    Update ex2.9.c

--- ex2.9.c
+++ ex2.9.c
@@ -31,6 +31,6 @@ int main()
  * For any odd number, x-1, turns off the first bit which corresponds to 2^0.
  * For any even number, x-1, turns on all bits to the right of the least significant bit of x, and
  * turns off the least significant bit of x. See example 1.
- * Thus, taking an and of x & x-1 now turns of the least significant bit of x and leaves the rest 
+ * Thus, taking an and of x & x-1 now turns off the least significant bit of x and leaves the rest 
  * bits untouched.
  */
